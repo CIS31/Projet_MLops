b84ce1b1dfb68c30c8f1992261b10b5f4e46e718
romainDonne22 <rom.donne@gmail.com>
2025-03-24 15:43:13 +0100

    test_airflow ok

--- tests/test_airflow.py
+++ tests/test_airflow.py
@@ -14,7 +14,7 @@ DB_CONN = {
     "dbname": "airflow",
     "user": "airflow",
     "password": "airflow",
-    "host": "postgres",
+    "host": "0.0.0.0",
     "port": 5432,
 }
 
@@ -45,24 +45,25 @@ def test_task_execution(dag, db_connection):
         cursor.execute("SELECT 1;")
         result = cursor.fetchall()
         assert len(result) > 0, "La connexion à la base de données a échoué."
+        print("Connexion à la base de données réussie.")
 
     # Créer un DagRun pour le DAG
     execution_date = pendulum.datetime(2025, 3, 22, tz="UTC")  # Ajout du fuseau horaire
     with db_connection.cursor() as cursor:
         cursor.execute(
             """
-            INSERT INTO dag_run (dag_id, run_id, run_type, execution_date, state)
-            VALUES (%s, %s, %s, %s, %s)
+            INSERT INTO dag_run (dag_id, run_id, run_type, execution_date, state, clear_number)
+            VALUES (%s, %s, %s, %s, %s, %s)
             RETURNING id;
             """,
-            ("test_dag", "test_run", DagRunType.MANUAL, execution_date, State.RUNNING),
+            ("test_dag", "test_run", DagRunType.MANUAL, execution_date, State.RUNNING, 0),
         )
         dag_run_id = cursor.fetchone()[0]
         db_connection.commit()
 
     # Créer une instance de la tâche
     task = dag.get_task("dummy_task")
-    task_instance = TaskInstance(task=task, execution_date=execution_date)
+    task_instance = TaskInstance(task=task, run_id=f"manual__{execution_date.isoformat()}")
     task_instance.dag_run_id = dag_run_id
 
     # Exécuter la tâche et vérifier son état
